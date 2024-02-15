394d68d4285b4b2ccfca95cebe6cd95bc08419a3
virtuxa <vidim2004@gmail.com>
2024-02-16 00:20:01 +0300

    Что то творим

--- app.py
+++ app.py
@@ -8,51 +8,27 @@ from PyQt6.QtWebEngineWidgets import *
 
 logger = logging.getLogger("module.app")
 
+# Запускаем работу приложения
 def main():
     logger.info("Start loading app module")
     app=QApplication(sys.argv)
     win=MainWindow()
     win.show()
-    logger.debug("Show window")
     app.exit(app.exec())
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        self.setWindowTitle('Пример загрузки локальной веб-страницы')
-        self.setGeometry(5,30,1355,730)
-        self.browser=QWebEngineView()
-        
-        # # Загрузить внешний веб-интерфейс
-        # url=r'index.html'
-        # self.browser.load(QUrl(url))
 
-        self.browser.setHtml('''<!DOCTYPE html>
-                                <html>
-                                    <head>
+        self.setWindowTitle('GPZ')
+        self.setGeometry(50,50,1116,851)
+        self.setMinimumSize(900,900)
 
-                                        <script src="https://api-maps.yandex.ru/2.1/?lang=ru-RU" type="text/javascript"></script>
+        view = QWebEngineView()
+        view.setHtml(open("yaMap.html").read())
 
-                                    </head>
-                                    <body>
-                                        
-                                        <div id="myMap" style="width: 700px; height: 700px;"></div>
+        self.setCentralWidget(view)
 
-                                        <script>
-                                            ymaps.ready(init);
-                                            function init(){
-                                                var myMap = new ymaps.Map ('myMap', {
-                                                    center: [59.850286, 30.237357],
-                                                    zoom: 16,
-                                                    type: 'yandex#satellite'
-                                                });
-                                            }
-
-                                        </script>
-
-                                    </body>
-                                </html>''')
-        self.setCentralWidget(self.browser)
 
 if __name__ == '__main__':
     main()
