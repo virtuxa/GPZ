06d4773550142c8b885f07aafecbce9bd08458d4
virtuxa <vidim2004@gmail.com>
2024-02-16 19:51:41 +0300

    Добавлен и улучшен функционал приложения

--- app.py
+++ app.py
@@ -5,17 +5,25 @@ from PyQt6.QtCore import *
 from PyQt6.QtGui import *
 from PyQt6.QtWidgets import *
 from PyQt6.QtWebEngineWidgets import *
-from PyQt6.QtWidgets import QTextEdit
 
 logger = logging.getLogger("module.app")
 
 # Запускаем работу приложения
 def main():
     logger.info("Start loading app module")
-    app=QApplication(sys.argv)
-    win=MainWindow()
+    app = QApplication(sys.argv)
+    win = MainWindow()
     win.show()
     app.exit(app.exec())
+
+    # Темная тема
+    # logger.info("Start loading app module")
+    # sys.argv += ['-platform', 'windows:darkmode=2']
+    # app = QApplication(sys.argv)
+    # app.setStyle('Fusion')
+    # win = MainWindow()
+    # win.show()
+    # app.exit(app.exec())
 
 class Color(QWidget):
 
@@ -31,36 +39,85 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
 
+        # Настройки главного окна
         self.setWindowTitle('GPZ')
-        self.setGeometry(50,50,1200,900)
-        self.setMinimumSize(900,900)
+        self.setGeometry(50, 50, 1200, 900)
+        self.setMinimumSize(960, 920)
 
         yaMap = QWebEngineView()
         yaMap.setHtml(open("yaMap.html").read())
 
-        layout1 = QHBoxLayout()
-        layout2 = QVBoxLayout()
-        layout3 = QHBoxLayout()
+        # Это кнопки
+        butNew = QPushButton("")
+        butNew.setFixedSize(60, 60)
+        butNew.setIcon(QIcon('icons/butNew.png'))
+        butNew.setIconSize(QSize(60, 60))
+        butNew.clicked.connect(self.close)
 
-        layout1.addWidget(QPushButton("123"))
-        layout1.addWidget(QPushButton("123"))
-        layout1.addWidget(QPushButton("123"))
+        butSave = QPushButton("")
+        butSave.setFixedSize(60, 60)
+        butSave.setIcon(QIcon('icons/butSave.png'))
+        butSave.setIconSize(QSize(60, 60))
+        butSave.clicked.connect(self.close)
 
-        layout2.addLayout( layout1 )
+        butFile = QPushButton("")
+        butFile.setFixedSize(60, 60)
+        butFile.setIcon(QIcon('icons/butFile.png'))
+        butFile.setIconSize(QSize(60, 60))
+        butFile.clicked.connect(self.close)
+
+        butSett = QPushButton("")
+        butSett.setFixedSize(60, 60)
+        butSett.setIcon(QIcon('icons/butSett.png'))
+        butSett.setIconSize(QSize(60, 60))
+        butSett.clicked.connect(self.close)
+
+        butPoin = QPushButton("")
+        butPoin.setFixedSize(60, 60)
+        butPoin.setIcon(QIcon('icons/butPoin.png'))
+        butPoin.setIconSize(QSize(60, 60))
+        butPoin.clicked.connect(self.close)
+
+        butObje = QPushButton("")
+        butObje.setFixedSize(60, 60)
+        butObje.setIcon(QIcon('icons/butObje.png'))
+        butObje.setIconSize(QSize(60, 60))
+        butObje.clicked.connect(self.close)
+
+        butTrek = QPushButton("")
+        butTrek.setFixedSize(60, 60)
+        butTrek.setIcon(QIcon('icons/butTrek.png'))
+        butTrek.setIconSize(QSize(60, 60))
+        butTrek.clicked.connect(self.close)
+
+        vertLayoutMain = QVBoxLayout()
+        horLayout1 = QHBoxLayout()
+        horLayout2 = QHBoxLayout()
+
+        horLayout1.addWidget(butNew)
+        horLayout1.addWidget(butSave)
+        horLayout1.addWidget(butFile)
+        horLayout1.addWidget(butSett)
+        horLayout1.addWidget(butPoin)
+        horLayout1.addWidget(butObje)
+        horLayout1.addWidget(butTrek)
+
+        horLayout1.addStretch()
+
+        vertLayoutMain.addLayout(horLayout1)
 
         text = QTextEdit()
-        text.setGeometry(1500,1500,1500,1500)
-        text.setPlaceholderText("123")
+        text.setPlaceholderText("Текст текст")
 
-        layout3.addWidget(yaMap)
-        layout3.addWidget(text)
+        horLayout2.addWidget(yaMap)
+        horLayout2.addWidget(text)
 
-        layout2.addLayout( layout3 )
-        layout2.setSpacing(10)
-        layout2.setContentsMargins(28,28,28,28)
+        vertLayoutMain.addLayout(horLayout2)
+        vertLayoutMain.setSpacing(10)
+        vertLayoutMain.setContentsMargins(28, 28, 28, 28)
 
         widget = QWidget()
-        widget.setLayout(layout2)
+        widget.setLayout(vertLayoutMain)
         self.setCentralWidget(widget)
 
 
