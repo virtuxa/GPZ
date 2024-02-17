3ffe393684b2616ba9452a2e1d554e16d3a34c59
virtuxa <vidim2004@gmail.com>
2024-02-17 15:22:26 +0300

    Исправлена архитектура приложения

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ logger = logging.getLogger("module.app")
 def main():
     logger.info("Start loading app module")
     app = QApplication(sys.argv)
-    app.setStyleSheet("MainWindow{background-color: #D9D9D9;}")
+    app.setStyleSheet("MainWindow{background-color: #e8e8e8;}") #D9D9D9
     win = MainWindow()
     win.show()
     app.exit(app.exec())
@@ -47,91 +47,100 @@ class MainWindow(QMainWindow):
 
         yaMap = QWebEngineView()
         yaMap.setHtml(open("yaMap.html").read())
-        yaMap.setStyleSheet('border-radius: 50;background-color: #D9D9D9;')
+        yaMap.setStyleSheet('border-radius: 5;background-color: #e8e8e8;')
 
         # Создание и настройка кнопок функционального меню
         butNew = QPushButton("")
         butNew.setIcon(QIcon('icons/butNew.png'))
         butNew.setIconSize(QSize(65, 65))
         butNew.clicked.connect(self.close)
-        butNew.setStyleSheet('border-radius: 50;background-color: #D9D9D9;')
+        butNew.setStyleSheet('border-radius: 50;background-color: #e8e8e8;')
 
         butSave = QPushButton("")
         butSave.setIcon(QIcon('icons/butSave.png'))
         butSave.setIconSize(QSize(65, 65))
         butSave.clicked.connect(self.close)
-        butSave.setStyleSheet('border-radius: 50;background-color: #D9D9D9;')
+        butSave.setStyleSheet('border-radius: 50;background-color: #e8e8e8;')
 
         butFile = QPushButton("")
         butFile.setIcon(QIcon('icons/butFile.png'))
         butFile.setIconSize(QSize(65, 65))
         butFile.clicked.connect(self.close)
-        butFile.setStyleSheet('border-radius: 50;background-color: #D9D9D9;')
+        butFile.setStyleSheet('border-radius: 50;background-color: #e8e8e8;')
 
         butSett = QPushButton("")
         butSett.setIcon(QIcon('icons/butSett.png'))
         butSett.setIconSize(QSize(65, 65))
         butSett.clicked.connect(self.close)
-        butSett.setStyleSheet('border-radius: 50;background-color: #D9D9D9;')
+        butSett.setStyleSheet('border-radius: 50;background-color: #e8e8e8;')
 
         butPoin = QPushButton("")
         butPoin.setIcon(QIcon('icons/butPoint.png'))
         butPoin.setIconSize(QSize(130, 65))
         butPoin.clicked.connect(self.close)
-        butPoin.setStyleSheet('border-radius: 50;background-color: #D9D9D9;')
+        butPoin.setStyleSheet('border-radius: 50;background-color: #e8e8e8;')
 
         butObje = QPushButton("")
         butObje.setIcon(QIcon('icons/butObject.png'))
         butObje.setIconSize(QSize(130, 65))
         butObje.clicked.connect(self.close)
-        butObje.setStyleSheet('border-radius: 50;background-color: #D9D9D9;')
+        butObje.setStyleSheet('border-radius: 50;background-color: #e8e8e8;')
 
         butTrek = QPushButton("")
         butTrek.setIcon(QIcon('icons/butTrek.png'))
         butTrek.setIconSize(QSize(130, 65))
         butTrek.clicked.connect(self.close)
-        butTrek.setStyleSheet('border-radius: 50;background-color: #D9D9D9;')
+        butTrek.setStyleSheet('border-radius: 50;background-color: #e8e8e8;')
 
-        # Создание 3-ех слоев
-        vertLayoutMain = QVBoxLayout()
-        horLayout1 = QHBoxLayout()
-        horLayout2 = QHBoxLayout()
-
-        # Добавление кнопок на слой
-        horLayout1.addWidget(butNew)
-        horLayout1.addWidget(butSave)
-        horLayout1.addWidget(butFile)
-        horLayout1.addWidget(butSett)
-
-        # Привязка кнопок к левой стороне
-        horLayout1.addStretch(2)
-
-        horLayout1.addWidget(butPoin)
-        horLayout1.addWidget(butObje)
-        horLayout1.addWidget(butTrek)
-
-        vertLayoutMain.addLayout(horLayout1)
-
+        # Создание выходных данных для пользователя
         outlog = QLabel()
         outlog.setStyleSheet("border-radius: 10;background-color: white;")
         outlog.setAlignment(Qt.AlignmentFlag(1))
         outlog.setFont(QFont('Arial', 15))
+        curOutput = outlog.text()
+        outlog.setText(curOutput + '\n 123')
 
-        horLayout2.addWidget(yaMap,3)
-        horLayout2.addWidget(outlog,1)
+        # Создание структурных слоёв приложения
+        layoutMain = QHBoxLayout()
+        layoutSTLeft = QVBoxLayout()
+        layoutSTRight = QVBoxLayout()
 
-        vertLayoutMain.addLayout(horLayout2)
-        vertLayoutMain.setSpacing(10)
-        vertLayoutMain.setContentsMargins(28, 28, 28, 28)
+        layoutSTLeftUp = QHBoxLayout()
+        layoutSTLeftDown = QHBoxLayout()
+        layoutSTRightUp = QHBoxLayout()
+        layoutSTRightDown = QHBoxLayout()
+        
+        # Добавление кнопок
+        layoutSTLeftUp.addWidget(butNew)
+        layoutSTLeftUp.addWidget(butSave)
+        layoutSTLeftUp.addWidget(butFile)
+        layoutSTLeftUp.addWidget(butSett)
+        layoutSTLeftUp.addStretch(1)
+
+        layoutSTRightUp.addWidget(butPoin)
+        layoutSTRightUp.addWidget(butObje)
+        layoutSTRightUp.addWidget(butTrek)
+
+        layoutSTLeftDown.addWidget(yaMap,3)
+        layoutSTRightDown.addWidget(outlog,1)
+
+        layoutSTLeft.addLayout(layoutSTLeftUp)
+        layoutSTLeft.addLayout(layoutSTLeftDown)
+        layoutSTRight.addLayout(layoutSTRightUp)
+        layoutSTRight.addLayout(layoutSTRightDown)
+
+        layoutMain.addLayout(layoutSTLeft,3)
+        layoutMain.addLayout(layoutSTRight,1)
+        
+        # Настройка главного слоя
+        layoutMain.setSpacing(10)
+        layoutMain.setContentsMargins(28, 28, 28, 28)
 
         # Добавление общего виджета и вывод его на экран
         widget = QWidget()
-        widget.setLayout(vertLayoutMain)
+        widget.setLayout(layoutMain)
         self.setCentralWidget(widget)
 
 
 if __name__ == '__main__':
     main()
-
-# curOutput = outlog.text()
-# outlog.setText(curOutput + '\n 123')
