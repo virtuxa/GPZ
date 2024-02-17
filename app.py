6c62c98d4c76ec8d02aa60908c086d05d768a5fc
virtuxa <vidim2004@gmail.com>
2024-02-17 14:04:41 +0300

    Переработано отображение изображений кнопок, а также добавлена возможность отоброжения текущих логов для пользователя

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__()
 
         # Настройки главного окна
-        self.setWindowTitle('GPZ')
+        self.setWindowTitle('Генератор полётных заданий')
         self.setGeometry(50, 50, 1200, 900)
         self.setMinimumSize(960, 920)
 
@@ -50,46 +50,46 @@ class MainWindow(QMainWindow):
 
         # Создание и настройка кнопок функционального меню
         butNew = QPushButton("")
-        butNew.setFixedSize(60, 60)
         butNew.setIcon(QIcon('icons/butNew.png'))
-        butNew.setIconSize(QSize(60, 60))
+        butNew.setIconSize(QSize(65, 65))
         butNew.clicked.connect(self.close)
+        butNew.setStyleSheet('border-radius: 50;background-color: #D9D9D9;')
 
         butSave = QPushButton("")
-        butSave.setFixedSize(60, 60)
         butSave.setIcon(QIcon('icons/butSave.png'))
-        butSave.setIconSize(QSize(60, 60))
+        butSave.setIconSize(QSize(65, 65))
         butSave.clicked.connect(self.close)
+        butSave.setStyleSheet('border-radius: 50;background-color: #D9D9D9;')
 
         butFile = QPushButton("")
-        butFile.setFixedSize(60, 60)
         butFile.setIcon(QIcon('icons/butFile.png'))
-        butFile.setIconSize(QSize(60, 60))
+        butFile.setIconSize(QSize(65, 65))
         butFile.clicked.connect(self.close)
+        butFile.setStyleSheet('border-radius: 50;background-color: #D9D9D9;')
 
         butSett = QPushButton("")
-        butSett.setFixedSize(60, 60)
         butSett.setIcon(QIcon('icons/butSett.png'))
-        butSett.setIconSize(QSize(60, 60))
+        butSett.setIconSize(QSize(65, 65))
         butSett.clicked.connect(self.close)
+        butSett.setStyleSheet('border-radius: 50;background-color: #D9D9D9;')
 
         butPoin = QPushButton("")
-        butPoin.setFixedSize(60, 60)
-        butPoin.setIcon(QIcon('icons/butPoin.png'))
-        butPoin.setIconSize(QSize(60, 60))
+        butPoin.setIcon(QIcon('icons/butPoint.png'))
+        butPoin.setIconSize(QSize(130, 65))
         butPoin.clicked.connect(self.close)
+        butPoin.setStyleSheet('border-radius: 50;background-color: #D9D9D9;')
 
         butObje = QPushButton("")
-        butObje.setFixedSize(60, 60)
-        butObje.setIcon(QIcon('icons/butObje.png'))
-        butObje.setIconSize(QSize(60, 60))
+        butObje.setIcon(QIcon('icons/butObject.png'))
+        butObje.setIconSize(QSize(130, 65))
         butObje.clicked.connect(self.close)
+        butObje.setStyleSheet('border-radius: 50;background-color: #D9D9D9;')
 
         butTrek = QPushButton("")
-        butTrek.setFixedSize(60, 60)
         butTrek.setIcon(QIcon('icons/butTrek.png'))
-        butTrek.setIconSize(QSize(60, 60))
+        butTrek.setIconSize(QSize(130, 65))
         butTrek.clicked.connect(self.close)
+        butTrek.setStyleSheet('border-radius: 50;background-color: #D9D9D9;')
 
         # Создание 3-ех слоев
         vertLayoutMain = QVBoxLayout()
@@ -130,3 +130,6 @@ class MainWindow(QMainWindow):
 
 if __name__ == '__main__':
     main()
+
+# curOutput = outlog.text()
+# outlog.setText(curOutput + '\n 123')
