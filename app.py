79aaaeefc6f2c33ccc16c5d92a5f3d033ede28c8
virtuxa <dmitry-vidineeff@ya.ru>
2024-04-20 19:58:25 +0300

    Исправлены предыдущие недочёты, также добавлено отображение высоты, стрелок маркеров, добавлена возможность удаления маркеров двойным кликом

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from PyQt6.QtCore import *
 from PyQt6.QtGui import *
 from PyQt6.QtWidgets import *
 from PyQt6.QtWebEngineWidgets import *
+from PyQt6.QtWebChannel import QWebChannel
 
 logger = logging.getLogger("module.app")
 backgroundMainColor = '#e8e8e8'
@@ -19,10 +20,41 @@ def main():
     win.show()
     app.exit(app.exec())
 
+class Backend(QObject):
+    # Сигнал для передачи координат маркера
+    markerCoordinatesChanged = pyqtSignal(float, float)
+
+    # Слот для получения координат маркера из JavaScript
+    @pyqtSlot(float, float)
+    def receiveMarkerCoordinates(self, lat, lng):
+        # Отправляем сигнал с координатами маркера
+        self.markerCoordinatesChanged.emit(lat, lng)
+
 class MainWindow(QMainWindow):
 
+    # Слот для обработки полученных координат маркера
+    def handleMarkerCoordinatesChanged(self, lat, lng):
+        print(f"Received marker coordinates: Lat={lat}, Lng={lng}")
+        
     def __init__(self):
         super(MainWindow, self).__init__()
+
+        # Инициализируем html, а также создаём связь с js для обмена данными
+        self.map_int = QWebEngineView() # Создаем экземпляр QWebEngineView
+        # Создаем объекты канала веб-канала
+        self.channel = QWebChannel()
+        self.backend = Backend()
+        self.channel.registerObject('backend', self.backend) # Регистрируем объекты канала веб-канала
+        self.map_int.page().setWebChannel(self.channel) # Устанавливаем канал для QWebEngineView
+        # Загружаем HTML-файл в QWebEngineView
+        file_path = os.path.abspath('map/map.html')
+        self.map_int.load(QUrl.fromLocalFile(file_path))
+        self.backend.markerCoordinatesChanged.connect(self.handleMarkerCoordinatesChanged) # Подключаем сигнал от backend к слоту в MainWindow
+                
+        # map_int = QWebEngineView()
+        # map_int.load(QUrl.fromLocalFile(os.path.abspath('map/map.html')))
+        # map_int.setHtml(open("templates/map.html").read())
+        # map_int.load(QUrl('http://127.0.0.1:5000/'))
 
         # Настройки главного окна
         self.setWindowTitle('Генератор полётных заданий')
@@ -46,16 +78,11 @@ class MainWindow(QMainWindow):
         butSett = init_button("butSett", 65, 65)
         butSett.clicked.connect(lambda: log("Has detected event click button butSett", outlog))
         butPoint = init_button("butPoint", 130, 65)
-        butPoint.clicked.connect(lambda: showCoords("Has detected event click button butObject", outlog))
+        butPoint.clicked.connect(lambda: log("Has detected event click button butPoint", outlog))
         butObject = init_button("butObject", 130, 65)
         butObject.clicked.connect(lambda: log("Has detected event click button butObject", outlog))
         butTrek = init_button("butTrek", 130, 65)
         butTrek.clicked.connect(lambda: log("Has detected event click button butTrek", outlog))
-
-        map_int = QWebEngineView()
-        map_int.load(QUrl.fromLocalFile(os.path.abspath('map/map.html')))
-        # map_int.setHtml(open("templates/map.html").read())
-        # map_int.load(QUrl('http://127.0.0.1:5000/'))
 
         # Создание структурных слоёв приложения
         layoutMain = QHBoxLayout()
@@ -80,7 +107,7 @@ class MainWindow(QMainWindow):
         layoutSTRightUp.addWidget(butTrek)
 
         # Добавление карты и выходных логов пользователя
-        layoutSTLeftDown.addWidget(map_int,3)
+        layoutSTLeftDown.addWidget(self.map_int,3)
         layoutSTRightDown.addWidget(outlog,1)
 
         # Распределение столбцов по опорным слоям
@@ -121,11 +148,6 @@ def init_button(nameIcon, sizeX, sizeY):
     button.setStyleSheet('border-radius: 50;background-color: %s;'%backgroundMainColor)
 
     return button
-
-# Отображение координат при нажатии кнопки butPoint в окне output
-def showCoords(coordStart, coordEnd, outlog):
-    log(coordStart, outlog)
-    log(coordEnd, outlog)
 
 if __name__ == '__main__':
     main()
--- map/script.js
+++ map/script.js
@@ -5,7 +5,7 @@ var map = L.map('map',{
     max_zoom: 16,
 }).setView([59.946991,30.237421]);
 
-// Добавление базового слоя
+// Добавление двух слоёв (vector и satellite)
 L.tileLayer('G:/System/YandexIgnore/Личные документы/Проекты/НПП Радар/GPZ/map/vector/{z}/{x}/{y}.png', {
     attribution: 'vector',
     min_zoom: 12,
@@ -21,8 +21,87 @@ L.tileLayer('G:/System/YandexIgnore/Личные документы/Проект
     maxNativeZoom: 16
 }).addTo(map);
 
-// Печать координат в консоль на ПКМ
-map.on("contextmenu", function (event) {
-    console.log("Coordinates: " + event.latlng.toString());
-    console.log(map.getZoom())
+/* НАСТРОЙКА КАРТЫ */
+L.control.mousePosition().addTo(map); // Отображение координат на карте
+L.control.scale().addTo(map); // Отображение высоты на карте
+map.attributionControl.setPrefix('<img src="../map/assets/flag.png" height="14px" width="14px">'); // Смена аттрибутов на карте
+map.doubleClickZoom.disable(); // Отключение приближения при двойном клике
+
+/* Отображение обьектов на карте */
+// Инициализация необходимых переменных
+var markers = []; // Переменная для хранения маркеров
+var lines = []; // Переменная для хранения линий
+
+// Зададим настройки обьектов
+var wayPointIcon = L.icon({ // Настройка иконки маркера
+    iconUrl: '../map/assets/wayPoint.png', // URL картинки
+    iconSize: [36, 40], // Размеры картинки в пикселях
+    iconAnchor: [18, 40], // Точка, которая будет соответствовать точке на земле (относительно величины картинки)
+    popupAnchor: [0, -20] // Точка относительно iconAnchor, где будет открываться всплывающее окно (X, Y)
 });
+var lineOptions = { // Настройка линии
+    color: 'purple',
+    weight: 3,
+};
+var arrowheadsOptions = { // Настройка стрелки
+    frequency: 2,
+    size: '15px',
+}
+var markerOptions = { // Настройка маркеров
+    draggable: true,
+    icon: wayPointIcon
+}
+
+// Зададим настройки QWebChannel
+new QWebChannel(qt.webChannelTransport, function (channel) {
+    window.backend = channel.objects.backend;
+});
+
+// Функция для добавления маркеров и соединения их линиями
+function addMarkerAndConnect(e) {
+    backend.receiveMarkerCoordinates(e.latlng.lat, e.latlng.lng); // Возвращает значения координат
+
+    var marker = L.marker(e.latlng, markerOptions).addTo(map);
+    marker.bindPopup(e.latlng.toString()).openPopup(); // Отображение координат во всплывающем окне маркера
+    markers.push(marker);
+    
+    if (markers.length > 1) {
+        var prevMarker = markers[markers.length - 2].getLatLng();
+        var currentMarker = marker.getLatLng();
+        var line = L.polyline([prevMarker, currentMarker], lineOptions).arrowheads(arrowheadsOptions).addTo(map);
+        lines.push(line);
+    }
+        
+    marker.on('move', updateLines); // Обновление линий при перемещении маркера
+    marker.on('dblclick', function() { // Обработчик события для удаления последнего маркера при двойном клике
+        deleteLastMarker();
+    });
+}
+// Функция для обновления позиции линий при перемещении маркера
+function updateLines(e) {
+    var markerIndex = markers.indexOf(e.target);
+    if (markerIndex > -1) {
+        if (markerIndex > 0) {
+            var prevMarker = markers[markerIndex - 1].getLatLng();
+            var currentMarker = e.target.getLatLng();
+            lines[markerIndex - 1].setLatLngs([prevMarker, currentMarker]);
+        }
+        if (markerIndex < markers.length - 1) {
+            var nextMarker = markers[markerIndex + 1].getLatLng();
+            var currentMarker = e.target.getLatLng();
+            lines[markerIndex].setLatLngs([currentMarker, nextMarker]);
+        }
+    }
+}
+// Функция для удаления последнего маркера
+function deleteLastMarker() {
+    if (markers.length > 0) {
+        var lastMarker = markers.pop(); // Удаление последнего маркера из массива
+        var lastLine = lines.pop(); // Удаление последней линии из массива
+        map.removeLayer(lastMarker); // Удаление маркера с карты
+        map.removeLayer(lastLine); // Удаление линии с карты
+    }
+}
+
+// Обработчик события клика по карте
+map.on('click', addMarkerAndConnect);
